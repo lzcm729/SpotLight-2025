c4bdf20ecd774e03345dd937158d43fe702cd20e
ByronZyh <43203171+ByronZyh@users.noreply.github.com>
2025-06-22 15:39:05 +0800

    游戏目标

--- Levels/MainLevel/GameGoal.gd
+++ Levels/MainLevel/GameGoal.gd
@@ -2,10 +2,19 @@ extends Control
 class_name GameGoal
 
 signal game_goal_closed
+@onready var v_box_container: VBoxContainer = $Bg/VBoxContainer
+@onready var v_box_container_2: VBoxContainer = $Bg/VBoxContainer2
 
 
 func show_game_goal():
 	#get_tree().paused = true
+	v_box_container.visible = true
+	v_box_container_2.visible = false
+	self.visible = true
+	
+func show_game_goal_2():
+	v_box_container.visible = false
+	v_box_container_2.visible = true
 	self.visible = true
 
 
