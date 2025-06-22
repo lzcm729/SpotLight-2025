e398317c2a7058d5447fd7172b174ed79ad584f6
lzcm729 <lzcm.729@gmail.com>
2025-06-22 11:57:04 +0800

    飞船血量为0游戏失败

--- Levels/MainLevel/MainLevel.gd
+++ Levels/MainLevel/MainLevel.gd
@@ -3,9 +3,12 @@ extends Node2D
 signal level_lost
 
 @onready var black_hole = $Universe/BlackHole
+@onready var space_ship = $Universe/SpaceShip
+
 
 func _ready() -> void:
 	black_hole.ship_gone.connect(_on_ship_gone)
+	space_ship.health_depleted.connect(_on_ship_gone)
 	
 	
 func _on_ship_gone() -> void:
